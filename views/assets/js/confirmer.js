d3dffe0e5e3285a701b9f603a1aa0a41b1d6c63e
Andrew Brown <drusepth@gmail.com>
2015-12-29 14:41:39 -0600

    Example text

--- views/assets/js/confirmer.js
+++ views/assets/js/confirmer.js
@@ -8,10 +8,10 @@ $(document).ready(function () {
 		$.get('/proof', {question: question_field.val()}, function (response) {
 			results_table.find('tbody').html('');
 			results_table.find('thead').show();
-			
+
 			if (response.length == 0) {
 				results_table.find('thead').hide();
-				var cancel_text = "I'm sorry, but HL3 has been cancelled. Please ask something else.";
+				var cancel_text = "I'm sorry, but HL3 has been cancelled. Please ask something else, like 'Is HL3 coming out?'";
 				results_table.append($('<tr />').append($('<td />').text(cancel_text)));
 			}
 
