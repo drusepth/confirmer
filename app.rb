edd373d090c094acd550512a74dfab1c8e4a907f
Andrew Brown <drusepth@gmail.com>
2015-12-29 14:27:17 -0600

    Things should work now

--- app.rb
+++ app.rb
@@ -2,6 +2,166 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-get "/" do
+require 'net/http'
+require 'uri'
+
+class Pathfinder
+  attr_reader :data
+
+  def initialize data:
+    @data = data
+  end
+
+  def search goal:, method: :bfs
+    queue = []
+    # Allow all starting positions
+    data.keys.each do |metric|
+      next if data[metric].zero?
+      data[metric] = Integer(data[metric]) if data[metric] == Integer(data[metric])
+      queue << {value: data[metric], operation_in: "#{data[metric]} #{metric}", metric: metric, parent: nil, distance: 0}
+    end
+
+    while queue.any?
+      current = case method
+        when :bfs
+          queue.shift
+        when :dfs
+          queue.pop
+      end
+
+      if current[:value] == goal
+        path = [current]
+        while !current[:parent].nil?
+          path << current[:parent]
+          current = current[:parent]
+        end
+        return path.reverse
+      end
+
+      neighbors = adjacent_neighbors(from: current).select { |neighbor| valid_transition?(current, neighbor, queue, goal) }
+      queue.concat neighbors
+    end
+
+    return []
+  end
+
+  def valid_transition? current, neighbor, queue, goal
+    !invalid_transition? current, neighbor, queue, goal
+  end
+
+  def invalid_transition? current = {value: 0, metric: nil}, neighbor, queue, goal
+    [
+      queue.any? { |node| neighbor[:value].to_f == node[:value].to_f },
+      queue.any? { |node| neighbor[:metric] == node[:metric] },
+      neighbor[:value] == current[:value],
+      neighbor[:metric] == current[:metric],
+      neighbor[:value].zero?,
+      neighbor[:value] < 0,
+      neighbor[:value] > goal * 15,
+      neighbor[:distance] > 10
+    ].any?
+  end
+
+  private
+
+  def valid_operations lhs:, rhs:, suffix:
+    [
+      {
+        operation: Proc.new { lhs * rhs },
+        description: "#{lhs} * #{rhs}#{suffix.end_with?('percentage') ? '%' : ''} #{suffix} = #{lhs * rhs}"
+      },
+      {
+        operation: rhs.zero? ? Proc.new { 0 } : Proc.new { lhs.to_f / rhs.to_f },
+        description: "#{lhs} / #{rhs}#{suffix.end_with?('percentage') ? '%' : ''} #{suffix} = #{rhs.zero? ? 0 : lhs.to_f / rhs.to_f}"
+      },
+      {
+        operation: Proc.new { lhs + rhs },
+        description: "#{lhs} + #{rhs}#{suffix.end_with?('percentage') ? '%' : ''} #{suffix} = #{lhs + rhs}"
+      },
+      {
+        operation: Proc.new { lhs - rhs },
+        description: "#{lhs} - #{rhs}#{suffix.end_with?('percentage') ? '%' : ''} #{suffix} = #{lhs - rhs}"
+      }
+    ]
+  end
+
+  def adjacent_neighbors from:
+    # from: {value: 5, operation_in: '5 words', parent: {value: 10, ...}, distance: 3}
+
+    neighbors = []
+    data.each do |metric, metric_value|
+      next unless metric_value.is_a?(Integer) || metric_value.is_a?(Float)
+
+      valid_operations(lhs: from[:value], rhs: metric_value, suffix: metric).each do |op|
+        result = op[:operation].call
+        result = Integer(result) if result == Integer(result)
+        neighbors << {
+          value:        result,
+          operation_in: op[:description],
+          metric:       metric,
+          parent:       from,
+          distance:     from[:distance] + 1
+        }
+      end
+    end
+
+    neighbors
+  end
+end
+
+class Dactyl
+  def self.analyze message
+    #todo mass params for just the metrics we want
+    uri = URI.parse("http://www.dactyl.in/api/v1/dactyl?text=#{message}")
+    http = Net::HTTP.new(uri.host, uri.port)
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+
+    begin
+      data = JSON.parse(response.body)["metrics"]   
+      data = sanitize data
+      data   
+    rescue
+      { # some default data
+        word_count: message.split(' ').length,
+        character_count: message.chars.length,
+        spaces: message.count(' ')
+      }
+    end
+  end
+
+  def self.sanitize hash
+    hash
+      .select { |key, value| value.is_a?(Integer) || value.is_a?(Float) }
+      .each   { |key, value| value *= 100 if key.end_with? 'percentage' }
+  end
+end
+
+get '/' do
 	erb :"apidocs"
 end
+
+get '/proof' do
+	content_type :json
+	question = params[:question]
+
+	data = Dactyl.analyze(question)
+	pf = Pathfinder.new data: data
+	steps = pf.search goal: 3, method: :bfs
+
+	if steps.any?
+		steps.map! { |step| "#{step[:operation_in].gsub('_', ' ')}"}
+		steps << "HL3 confirmed."
+	end
+
+	steps.to_json
+end
+
+
+
+
+
+
+
+
---
+++ views/assets/js/confirmer.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+	var results_table  = $('#results-table'),
+	    ask_button     = $('#ask-button'),
+	    question_field = $('#question');
+
+	var ask_function = function () {
+		ask_button.text('Asking...');
+		$.get('/proof', {question: question_field.val()}, function (response) {
+			results_table.find('tbody').html('');
+			results_table.find('thead').show();
+			
+			if (response.length == 0) {
+				results_table.find('thead').hide();
+				var cancel_text = "I'm sorry, but HL3 has been cancelled. Please ask something else.";
+				results_table.append($('<tr />').append($('<td />').text(cancel_text)));
+			}
+
+			for (i = 0; i < response.length; i++) {
+				var row = $('<tr />').append($('<td />').text(response[i]));
+				results_table.append(row);
+			}
+			results_table.show();
+			ask_button.text('Ask');
+		}, "json");
+	}
+
+	// Trigger bindings
+	ask_button.click(function () { ask_function(); return false; });
+	question_field.keyup(function (e) { if (e.keyCode == 13) { ask_function(); } });
+});
